01a993ccd2cc71f463fa3aff7b604060d4712929
Mike Cowley <>
2018-03-04 22:32:54 -0700

    Pages: Civil Air Patrol, Education Videos Page, Gail's Videos Page, Contribute Page.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 
+//Stop Video When Modal Closes
+$(document).ready(function(){
+    $('.modal').each(function(){
+        var src = $(this).find('iframe').attr('src');
+
+        $(this).on('click', function(){
+
+            $(this).find('iframe').attr('src', '');
+            $(this).find('iframe').attr('src', src);
+
+        });
+    });
+});
