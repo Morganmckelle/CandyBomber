c713b0c739014fbf3a25b7b0224d7b4b9ae01c94
Morgan Hansen <morganmckelle@gmail.com>
2018-03-06 14:05:41 -0700

    updates

---
+++ header.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link type="image/png" rel="icon" href="img/logo/favicon.png" >
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+
+<!--------- NAVIGATION- -------->
+    <nav class="navbar navbar-default">
+		<div class="container">
+        <div class="container nav-first-level">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="img/logo_colors_crop.png" alt="Gail S. Halvorsen Aviation Education Foundation Logo"/>
+                </a>
+            </div>
+        </div>
+		</div>
+        <div class="nav-second-level">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<div class="container">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="/about_overview.html">ABOUT</a>
+                    </li>
+                    <li>
+                        <a href="contribute.html">CONTRIBUTE</a>
+                    </li>
+                    <li>
+                        <a href="educators overview.html">EDUCATORS</a>
+                    </li>
+                    <li>
+                        <a href="#">EVENTS</a>
+                    </li>
+                    <li>
+                        <a href="gail_overview.html">GAIL'S STORY</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">CONTACT</a>
+                    </li>
+                </ul>
+				</div>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
