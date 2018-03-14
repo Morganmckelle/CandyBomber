0ae8aea53d253a95524f3e4440aba33ad8b2d6b8
Morgan Hansen <morganmckelle@gmail.com>
2018-03-14 17:29:25 -0600

    contribute page

---
+++ contribute.php
@@ -0,0 +1,95 @@
+<?php require_once('header.php'); ?>
+
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-angle-up"></i>
+
+</button>
+
+
+    <!-- Overview -->
+	<div class="container-fluid">
+
+    <div class="container">
+        <div class="mission">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2 class="header2">Contribute</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+	<div class="container" style="padding: 0 60px;">
+		<div class="col-sm-12">
+            <h3 class="blueHeader text-center">Donate to help the foundation take flight</h4>
+            <!--<p>Donate to help the foundation take flight</p>-->
+            
+            
+        </div>
+        
+        <p>
+            Few episodes of history bring smiles and touch hearts as effectively as The Candy Bomber. Perhaps you are aware of Gail S. Halvorsen, who brought hope to needy children by dropping candy tied to tiny parachutes during the post-war Berlin Airlift in 1948-1949. The Gail S. Halvorsen Aviation Education Foundation promotes values which give rise to acts of service, and provides educational opportunities for children to experience science, technology, engineering, and math in a practical and hands-on fashion.
+        </p>
+		<div class="col-md-12" style="margin-top: 50px; margin-bottom: 50px;">
+                <a class="btn btn-primary btn-sm AboutButtonBlue center-block">DONATE</a>
+			<hr class="content-line">
+            </div>
+		
+		<div class="col-sm-12">
+            <h3 class="blueHeader text-center">Add your own touch and Buy a Brick</h4>
+		</div>
+		<p>
+            Place your name in a place of honor and help build the Gail S. Halvorsen Aviation Education Center at the Spanish Fork-Springville Airport</p>
+        <p>
+            Project: Your personalized brick will be featured in an outdoor plaza at the Gail S. Halvorsen Aviation Education Center. This 8,000 – 10,000 square foot facility will be built at the entrance to the Spanish Fork-Springville Airport. This facility will provide the platform for the Foundation’s initiatives, and will be one of the first educational enrichment facilities of its kind in the southern portion of Utah County. The purposes of this facility are:
+        </p>
+        <p>
+            - Provide space to house STEM educational enrichment events for local schools and private youth groups.
+        </p>
+        <p>
+            - Display memorabilia and artifacts which visually depict the Candy Bomber story to perpetuate the rich legacy of Gail S. Halvorsen’s remarkable career.
+        </p>
+        <p>
+            - Provide meeting space for like-minded organizations that support the Foundation’s education and humanitarian service missions
+        </p>
+        <p>
+            - Provide operating location for Civil Air Patrol in the central region of Utah.
+        </p>
+        <p>
+            - Support emergency response capacity in the region by providing an annex to the county emergency operations center, allowing expanded community participation in emergency or disaster response.
+        </p>
+        <p>
+            Join us in commemorating the 70th Anniversary of the Berlin Airlift and in honoring one of its heroes: Colonel Gail S. Halvorsen
+        </p>
+       
+       
+            <div class="col-md-12" style="margin-top:50px; margin-bottom: 50px">
+                <a class="btn btn-primary btn-sm AboutButtonBlue center-block">BUY A BRICK</a>
+				
+            </div>
+           
+        </div>
+    </div>
+
+    <div class="col-xs-12">
+        <hr class="content-line">
+    </div>
+
+
+        <div class="container" style="padding: 20px 40px;">
+            <div class="row">
+                <h2 class="header2 text-center" style="margin: 40px auto 20px auto;">Our Golden Donors</h2>
+            </div>
+            <div class="row text-center">
+                <p>Here are some of our highly honored Golden Donors and Contributors</p>
+            </div>
+            <div class="row">
+                <img class="img-responsive" src="img/golden-donors.png" alt="golden donors">
+            </div>
+        </div>
+
+
+</div> <!-- /container -->
+
+
+
+
+<?php require_once('footer.php'); ?>
